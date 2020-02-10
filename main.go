fcef6d0e35b1f6d5ea406f6454ec0b3851adf3cf
Stefan Nychka <stefannychka@gmail.com>
2020-02-10 16:09:09 -0700

    pass "too few" sub-test only

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ var errReadingInput = errors.New("Error reading input")
 
 func main() {
 
+	if len(os.Args) <= 1 {
+		printError(errInvalidArguments)
+	}
+
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
